79aa5903b400088fa3381824a3ab0cc44de12253
wminaung <minwin243@gmail.com>
2023-07-07 18:29:31 +0630

    add sqldump and add readme

---
+++ REDME.md
@@ -0,0 +1,21 @@
+check sql dump file form < /sql > folder
+create your own database LOL
+create tables like my table structure or just dosomething
+
+## if you done for data thing
+
+go admin page -- /onlineshop/admin <or> /onlineshop/admin/index.php
+
+# login with this username and passord
+
+```
+username : admin
+password : password
+
+```
+
+# customer page
+
+it's so easy just go -- /onlineshop <or> /onlineshop/index.php
+
+# thank for reading have fun
---
+++ sql/127_0_0_1.sql
@@ -0,0 +1,180 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 07, 2023 at 01:45 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `onlineshopping`
+--
+CREATE DATABASE IF NOT EXISTS `onlineshopping` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `onlineshopping`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_date` datetime NOT NULL,
+  `modified_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `created_date`, `modified_date`) VALUES
+(1, 'hoodie', '2023-07-07 18:01:05', '2023-07-07 18:01:05'),
+(2, 't-shirt', '2023-07-07 18:01:05', '2023-07-07 18:01:05'),
+(3, 'popular', '2023-07-07 18:03:33', '2023-07-07 18:03:33'),
+(4, 'shanpoo', '2023-07-07 18:03:33', '2023-07-07 18:03:33'),
+(5, 'recommend', '2023-07-07 18:08:46', '2023-07-07 18:08:46');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `brand` varchar(255) NOT NULL,
+  `review` varchar(225) NOT NULL,
+  `price` int(12) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `reached_date` datetime NOT NULL,
+  `expired_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `title`, `brand`, `review`, `price`, `photo`, `category_id`, `reached_date`, `expired_date`) VALUES
+(1, 'hello ', 'hello world', 'whahah', 200, 'spicyseafoodnoodle.png', 1, '2023-07-07 18:08:11', '2023-10-07 13:38:11'),
+(2, 'zella', 'sdfisdf', 'ssjadijf', 300, 'image 4.png', 5, '2023-07-07 18:09:05', '2023-10-07 13:39:05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `visa_card` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `status` int(1) NOT NULL,
+  `ordered_date` datetime NOT NULL,
+  `received_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `name`, `email`, `phone`, `visa_card`, `address`, `status`, `ordered_date`, `received_date`) VALUES
+(1, 'win', 'min@min', '1213', 'sdf', 'Dawei, 11th Street , No.209', 1, '2023-07-07 18:09:39', '2023-07-07 18:10:02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `qty` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`id`, `item_id`, `order_id`, `qty`) VALUES
+(1, 1, 1, 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
